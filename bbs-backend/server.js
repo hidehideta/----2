466724010c549bdd8295597778c5f95172d08e0f
hidehideta <222582878+hidehideta@users.noreply.github.com>
2025-11-16 22:48:45 +0900

    11/16のコメントアウト修正

--- bbs-backend/server.js
+++ bbs-backend/server.js
@@ -1,17 +1,24 @@
-// server.js
+// Node.jsでexpressを読み込んでいる。expressはWebサーバー(HTTPサーバー)を簡単に作るためのフレームワーク
 import express from "express";
+// CORSを許可するミドルウェアを読み込む 他のオリジンからのリクエストを許可するため
 import cors from "cors";
 
+// expressアプリケーションのインスタンスを作成
 const app = express();
+//読み込んだcorsミドルウェアをアプリ全体に適応
 app.use(cors());
+// JSONボディをパースするミドルウェアを適応　POSTリクエストのbodyをJSONとして扱うために必要
 app.use(express.json());
 
-let posts = []; // この例ではメモリ保存（再起動で消えます）
+/* 投稿データを保存するための配列　この例ではメモリ保存（再起動で消えます）
+実運用ではデータベースを用いる。*/
+let posts = []; 
 
-// 投稿一覧取得
+// 投稿一覧取得　Getリクエストに対して投稿データをJSON形式で返す
 app.get("/api/posts", (req, res) => {
   res.json(posts);
 });
+
 
 // 投稿追加
 app.post("/api/posts", (req, res) => {
